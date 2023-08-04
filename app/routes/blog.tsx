8cd0989b26fa8dd7abbfb22c2cdb7e04d791a472
gnobu <inyangubong17@gmail.com>
2023-08-04 20:49:34 +0100

    Added general title to blog route

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,7 +1,11 @@
-import { LinksFunction } from "@remix-run/node"
+import { LinksFunction, V2_MetaFunction } from "@remix-run/node"
 import { Outlet } from "@remix-run/react"
 
 import blogStylesUrl from "~/styles/blog.css"
+
+export const meta: V2_MetaFunction = () => {
+    return [{ title: "BLOG | UBONG" }]
+}
 
 export const links: LinksFunction = () => [
     { rel: "stylesheet", href: blogStylesUrl },
