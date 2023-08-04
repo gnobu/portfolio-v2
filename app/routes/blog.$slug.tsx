e25d1cffb69eb213ba3a8f7ac230ff6b32b3d7c8
gnobu <inyangubong17@gmail.com>
2023-08-04 22:12:05 +0100

    Added min to post duration

--- app/routes/blog.$slug.tsx
+++ app/routes/blog.$slug.tsx
@@ -39,7 +39,7 @@ export default function Article() {
                     <img className="post_image" src={article.image} alt="computer screen with code" width={350} />
                     <div className="">
                         <span className="flex jst-btwn al-end f-s-3">
-                            <span><span className="f-s-4 f-w-6">{article.tag}</span> | {article.length} read</span>
+                            <span><span className="f-s-4 f-w-6">{article.tag}</span> | {article.length} mins read</span>
                             <span className="col-border">{new Date(article.createdAt).toLocaleDateString()}</span>
                         </span>
                         <h1 className="title f-s-6 f-w-6">{article.title}</h1>
