2cd10b601a5a8d4b84db35350c76fb8edd9b2704
gnobu <inyangubong17@gmail.com>
2023-08-08 13:43:31 +0100

    create, update, delete fns for project

--- app/models/project.server.ts
+++ app/models/project.server.ts
@@ -1,3 +1,4 @@
+import { Project } from "@prisma/client"
 import { prisma } from "~/db.server"
 
 export async function getProjects() {
@@ -14,4 +15,21 @@ export async function getProjects() {
 
 export async function getProject(id: number) {
     return await prisma.project.findUnique({ where: { id } })
+}
+
+export async function createProject(data: Omit<Project, 'createdAt'>) {
+    return await prisma.project.create({
+        data
+    })
+}
+
+export async function updateProject(id: number, data: Partial<Project>) {
+    return await prisma.project.update({
+        where: { id },
+        data
+    })
+}
+
+export async function deleteProject(id: number) {
+    return await prisma.project.delete({ where: { id } })
 }
