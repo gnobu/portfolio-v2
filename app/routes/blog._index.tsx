70fb4be72ccff861e595740ae9bae945a5f6383c
gnobu <inyangubong17@gmail.com>
2023-08-09 23:43:10 +0100

    Feat: link to new article

--- app/routes/blog._index.tsx
+++ app/routes/blog._index.tsx
@@ -1,8 +1,10 @@
 import { json } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import { Link, useLoaderData } from "@remix-run/react"
+import { plusIcon } from "~/assets/icons"
 
 import ArticleCard from "~/components/ArticleCard"
 import IntoArticle from "~/components/IntoArticle"
+import SvgText from "~/components/SvgText"
 import Tag from "~/components/Tag"
 import { getArticles } from "~/models/blog.server"
 
@@ -17,25 +19,24 @@ export default function Blog() {
 
     return (
         <main className="m-blk-7">
-            <section className="container">
-                <aside className="p-blk-5">
-                    <ul className="flex f-wrap jst-center gap-1 no-list-style no-list-pad">
-                        {["All", "Css", "JavaScript", "React", "MongoDB", "Html"].map(tag => (
-                            <Tag key={tag} tagName={tag} />
-                        ))}
-                    </ul>
-                </aside>
-            </section>
-
+            <aside className="container p-blk-5">
+                <ul className="flex f-wrap jst-center gap-1 no-list-style no-list-pad">
+                    {["All", "Css", "JavaScript", "React", "MongoDB", "Html"].map(tag => (
+                        <Tag key={tag} tagName={tag} />
+                    ))}
+                </ul>
+            </aside>
             <section className="container m-blk-5">
                 {introArticle ? <IntoArticle article={{ ...introArticle, createdAt: new Date(introArticle.createdAt) }} /> : null}
             </section>
-
-            <section className="container other_posts m-blk-5">
+            <section className="container other_posts m-blk-6">
                 {articles.map(article => {
                     return <ArticleCard key={article.slug} article={{ ...article, createdAt: new Date(article.createdAt) }} />
                 })}
             </section>
+            <aside className="container m-blk-6 flex jst-center">
+                <Link to={'new'} title='Add article' className='button outline small '><SvgText src={plusIcon} srcCls='f-s-8' /></Link>
+            </aside>
         </main>
     )
 }
