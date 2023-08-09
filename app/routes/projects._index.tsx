af98ccfc453fe31249a3032adb61e092b8cf883b
gnobu <inyangubong17@gmail.com>
2023-08-09 23:40:03 +0100

    word change

--- app/routes/projects._index.tsx
+++ app/routes/projects._index.tsx
@@ -15,7 +15,7 @@ export default function Projects() {
     const { projects } = useLoaderData<typeof loader>()
     return (<>
         <header className="container m-blk-6">
-            <h1 className="f-s-9 f-w-3 col-sec">A showcase of my personal projects</h1>
+            <h1 className="f-s-9 f-w-3 col-sec">A showcase of my favorite projects</h1>
         </header>
         <section className="container projects m-blk-5">
             {projects.map(project => (<ProjectCard key={project.id} project={project} />))}
