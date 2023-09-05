3905c8fe84efd7bd3ea8d911d5d1dc06a6ec1351
gnobu <inyangubong17@gmail.com>
2023-09-05 11:32:55 +0100

    catch error when fetching blog

--- app/models/blog.server.ts
+++ app/models/blog.server.ts
@@ -2,20 +2,25 @@ import { Article } from "@prisma/client"
 import { prisma } from "~/db.server"
 
 export async function getArticles() {
-    return await prisma.article.findMany({
-        select: {
-            title: true,
-            image: true,
-            tag: true,
-            intro: true,
-            slug: true,
-            length: true,
-            createdAt: true,
-        },
-        orderBy: {
-            createdAt: 'desc'
-        }
-    })
+    try {
+        return await prisma.article.findMany({
+            select: {
+                title: true,
+                image: true,
+                tag: true,
+                intro: true,
+                slug: true,
+                length: true,
+                createdAt: true,
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 }
 
 export async function getArticle(slug: string) {
